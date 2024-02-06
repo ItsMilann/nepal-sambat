23b3a73a553e45aafe911d3d7ce0dd5a31577e7c
itsmilann <itsmilann@gitlab.com>
2024-02-06 15:04:14 +0545

    chore: various updates

--- nepal_sambat.py
+++ nepal_sambat.py
@@ -70,7 +70,7 @@ def _get_sun_time(dt:_TDateTime) -> tuple[_TDateTime, _TDateTime]:
 TITHI_LIST = ["पुिन", "पार", "िदतीया", "तृतीया", "चतुथी", "पञमी", "षषी", "सपमी", "अषमी", "नवमी",
               "दशमी", "एकादशी", "दादशी", "तयोदशी", "चतुदरशी", "आमै"]
 
-NS_MONTHS = ["कछला","िथंला","पँहेला","िसला","िचला","चौला","बछला","तछला","िदला","गुंला","ञला","कौला"]
+NS_MONTHS = ["कछला","िथंला","पोहेला","सिल्ला","चिल्ला","चौला","बछला","तछला","िदला","गुंला","ञला","कौला"]
 
 from skyfield.api import load
 from skyfield.framelib import ecliptic_frame
@@ -95,60 +95,70 @@ def _get_tithi(dt:datetime.datetime):
 
 
 def _get_month(dt: _TDateTime):
-    ...
+    ts = load.timescale()
+    t = ts.from_datetime(dt)
+    eph = load('de421.bsp')
+    astrometric = eph['Earth'].at(t).observe(eph['Sun'])
+    _, lon, _ = astrometric.frame_latlon(ecliptic_frame)
+    print(lon.degrees, lon.degrees / 30)
+    return round(lon.degrees / 30) - 8
+
+
+def _get_year(dt: _TDateTime, month):
+    DAYS_SINCE_KALIYUG = ts2j(dt.timestamp()) - 588465.5
+    KALI_YEAR = math.floor((DAYS_SINCE_KALIYUG + (10 - math.floor(month + 8)) * 30) / 365.25636)
+    SAKA_YEAR = KALI_YEAR - 3179
+    NS_YEAR = SAKA_YEAR - 801 # 2 is corrected value
+    print(NS_YEAR, "YEAR")
+    
 
 def get_nepal_sambat(dt:_TDateTime):
     SUNRISE, _ = _get_sun_time(dt)
     _JD = ts2j(dt.timestamp())
     _date = SUNRISE
+    _date = tz.localize(_date)
     print(_date.strftime("%Y/%m/%d"), end="\t")
     gatech.date = ephem.previous_new_moon(_date)
-    sun = ephem.Sun(gatech)
-    sunlon_at_new_moon = ephem.Ecliptic(sun).lon
-    month = round(math.degrees(sunlon_at_new_moon) / 30)
-    corrected_month = math.ceil(month) - 8
+    pnmd = ephem.previous_new_moon(_date).datetime()
+    month = _get_month(tz.localize(pnmd))
     tithi_today =  _get_tithi(dt)
     print("TITHI", tithi_today, end="\t")
     tithi_today = round(tithi_today)
-    tithi_yesterday = _get_tithi((dt - datetime.timedelta(days=1)))
-    tithi_yesterday = math.ceil(tithi_yesterday)
+    # tithi_yesterday = _get_tithi((dt - datetime.timedelta(days=1)))
+    # tithi_yesterday = math.ceil(tithi_yesterday)
     PAKSHYA = 1 if tithi_today <= 15 else 2
-    PAKSHYA_DEV = " (थ)" if PAKSHYA == 1 else " (गा)"
-    print(NS_MONTHS[corrected_month] + PAKSHYA_DEV,  end=", ")
+    PAKSHYA_DEV = "थ्व" if PAKSHYA == 1 else "गा"
+    print(NS_MONTHS[month] + PAKSHYA_DEV,  end=", ")
 
     if PAKSHYA == 2:
         tithi_today = tithi_today - 15
 
     if tithi_today in [15, 0]:
         tithi_today = 0 if PAKSHYA == 1 else 15
-        
+
     print(TITHI_LIST[tithi_today] + "\t" + str(tithi_today), end="\t")
 
-    if round(tithi_today) == round(tithi_yesterday):
-        print("\tT: ", 8, end="\t")
-    elif round(tithi_today) - round(tithi_yesterday) == 2:
-        print("\tT: ", 9, end="\t")
-    else:
-        print("\tT: ", 0, end="\t")
+    # if round(tithi_today) == round(tithi_yesterday):
+    #     print("\tT: ", 8, end="\t")
+    # elif round(tithi_today) - round(tithi_yesterday) == 2:
+    #     print("\tT: ", 9, end="\t")
+    # else:
+    #     print("\tT: ", 0, end="\t")
+    _get_year(dt, month)
 
-    DAYS_SINCE_KALIYUG = _JD - 588465.5
-    KALI_YEAR = math.floor((DAYS_SINCE_KALIYUG + (10 - math.floor(month)) * 30) / 365.25636)
-    SAKA_YEAR = KALI_YEAR - 3179
-    NS_YEAR = SAKA_YEAR - 799 # 2 is corrected value
-
-    print(NS_YEAR, "YEAR")
-
-err = 'error: required YYYY-MM-DD date format'
-parser = argparse.ArgumentParser(description='Optional app description')
-parser.add_argument('pos_arg', type=str, help=err)
-args = parser.parse_args()
-if not args:
-    print(err)
-_date = args.pos_arg
-try:
-    dt = datetime.date.fromisoformat(_date)
-except ValueError:
-    print(err)
-    sys.exit()
-dt = datetime.datetime.now().replace(year=dt.year, month=dt.month, day=dt.day)
-get_nepal_sambat(tz.localize(dt))
+if __name__ == "__main__":
+    err = 'error: required YYYY-MM-DD date format'
+    parser = argparse.ArgumentParser(description='Optional app description')
+    parser.add_argument('pos_arg', type=str, help=err)
+    args = parser.parse_args()
+    if not args:
+        print(err)
+    _date = args.pos_arg
+    try:
+        dt = datetime.date.fromisoformat(_date)
+    except ValueError:
+        print(err)
+        sys.exit()
+    dt = datetime.datetime.now().replace(year=dt.year, month=dt.month, day=dt.day)
+    dt = tz.localize(dt)
+    get_nepal_sambat(dt)
